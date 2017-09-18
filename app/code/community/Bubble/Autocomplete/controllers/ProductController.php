4dbc48f15cf80975d05ff9fff8ce65abb5e29471
Zander Holtberg <zholtberg@leedstone.com>
2017-09-17 21:33:23 -0500

    Refining fields used in JsonAction
    * This should hopefully reduce the payload size by excluding fields not
      used by the consuming JS.

--- app/code/community/Bubble/Autocomplete/controllers/ProductController.php
+++ app/code/community/Bubble/Autocomplete/controllers/ProductController.php
@@ -13,15 +13,49 @@ class Bubble_Autocomplete_ProductController extends Mage_Core_Controller_Front_A
     public function jsonAction()
     {
         $cacheId = 'bubble_autocomplete_' . Mage::app()->getStore()->getId();
-        if (false === ($data = Mage::app()->loadCache($cacheId))) {
+        $data = Mage::app()->loadCache($cacheId);
+        if (!$data) {
             $collection = Mage::getModel('catalog/product')->getCollection();
+            $collection->addAttributeToSelect('sku');
+            $collection->addAttributeToSelect('name');
+            $collection->addAttributeToSelect('price');
+            $collection->addAttributeToSelect('min_price');
+            $collection->addAttributeToSelect('final_price');
+            $collection->addAttributeToSelect('url_path');
+            $collection->addAttributeToSelect('image');
 
             Mage::dispatchEvent('bubble_autocomplete_product_collection_init', array('collection' => $collection));
 
-            $data = json_encode($collection->getData());
+            $newArray = array();
+            // Convert to JSON
+            $jsonData = json_encode($collection->getData());
+            $decoded = json_decode($jsonData);
+            foreach ($decoded as $d) {
+                if (!property_exists($d, 'sku') ||
+                    !property_exists($d, 'name') ||
+                    !property_exists($d, 'price') ||
+                    !property_exists($d, 'min_price') ||
+                    !property_exists($d, 'final_price') ||
+                    !property_exists($d, 'url_path') ||
+                    !property_exists($d, 'image')
+                ) {
+                    continue;
+                }
+                $newArray[] = array(
+                    'sku' => $d->sku,
+                    'name' => $d->name,
+                    'price' => $d->price,
+                    'min_price' => $d->min_price,
+                    'final_price' => $d->final_price,
+                    'image' => $d->image,
+                    'url_key' => $d->url_path
+                );
+            }
+
+            $finalJson = json_encode($newArray);
 
             $lifetime = Mage::helper('bubble_autocomplete')->getCacheLifetime();
-            Mage::app()->saveCache($data, $cacheId, array('block_html'), $lifetime);
+            Mage::app()->saveCache($finalJson, $cacheId, array('block_html'), $lifetime);
         }
 
         $this->getResponse()
